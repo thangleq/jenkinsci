8dfd158e9daa97d7b602ce24a30790c2043ce30c
thangleq <90259766+thangleq@users.noreply.github.com>
2021-09-08 09:09:43 +0700

    Update deployMe.groovy

--- vars/deployMe.groovy
+++ vars/deployMe.groovy
@@ -13,6 +13,15 @@ def call(Map pipelineParams) {
     echo helmRepo
     echo pipelineParams.serviceName
     echo serviceName
+    
+    podTemplate(name: ptNameVersion, label: ptNameVersion, containers: [
+        containerTemplate(name: 'builder', image: 'golang:1.10.3', ttyEnabled: true, command: 'cat', args: ''),
+        containerTemplate(name: 'docker', image: 'docker:17.09', ttyEnabled: true, command: 'cat', args: '' ),
+        containerTemplate(name: 'argo-cd-tools', image: 'argoproj/argo-cd-tools:latest', ttyEnabled: true, command: 'cat', args: '', envVars:[envVar(key: 'GIT_SSH_COMMAND', value: 'ssh -o StrictHostKeyChecking=no')] ),
+        containerTemplate(name: 'argo-cd-cli', image: 'argoproj/argocd-cli:v0.7.1', ttyEnabled: true, command: 'cat', args: '', envVars:[envVar(key: 'ARGOCD_SERVER', value: pipelineParams.argocdServer)] ),
+        ]
+      )
+    
     pipeline {
       agent any
       stages {
@@ -39,6 +48,17 @@ def call(Map pipelineParams) {
             }
           }
         }
+        
+        stage( "Deploy ArgoCD" ) 
+            container('argo-cd-cli') {
+                withCredentials([string(credentialsId: "argocd-deploy-role", variable: 'ARGOCD_AUTH_TOKEN')]) {
+                    # Customize image 
+                    sh "/argocd app set ${APP_NAME} -p image.tag=${gitCommit.GIT_COMMIT.take(7)}-${BUILD_NUMBER}"
+                    sh "/argocd app sync ${APP_NAME}"
+                    sh "/argocd app wait ${APP_NAME} --timeout 600"
+                }
+            }
+        }  
 //         stage('Check Release Notes condition') {
 //           steps{
 //             script {
