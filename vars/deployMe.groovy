5fb84f73f4d4fd6718081bb0f2ac9c424119bd5a
thangleq <90259766+thangleq@users.noreply.github.com>
2021-09-08 11:38:30 +0700

    Update deployMe.groovy

--- vars/deployMe.groovy
+++ vars/deployMe.groovy
@@ -2,17 +2,6 @@ def call(Map pipelineParams) {
     def registry = pipelineParams.registry
     def registryCredential = pipelineParams.registryCredential
     def dockerImage = ''
-    def helmRepo = pipelineParams.helmRepo
-    def helmPackage = pipelineParams.basePackage
-    def serviceName = pipelineParams.serviceName
-    def helmReleaseNote = 'release-notes'
-    def releaseNotes = pipelineParams.withReleaseNotes
-    def gitCommit = ''
-
-    echo pipelineParams.helmRepo
-    echo helmRepo
-    echo pipelineParams.serviceName
-    echo serviceName
     
     pipeline {
       agent any
@@ -24,14 +13,14 @@ def call(Map pipelineParams) {
             }
           }
         }
-        stage('Building image') {
+        stage('Building Image') {
           steps{
             script {
                 dockerImage = docker.build registry + ":"+ gitCommit.GIT_COMMIT.take(7)+"-${BUILD_NUMBER}"
             }
           }
         }
-        stage('Deploy Image') {
+        stage('Deploy Image to Registry') {
           steps{
             script {
               docker.withRegistry( 'https://' + registry, registryCredential ) {
@@ -53,35 +42,12 @@ def call(Map pipelineParams) {
             }
           } 
         }  
-//         stage('Check Release Notes condition') {
-//           steps{
-//             script {
-//                 if (releaseNotes.length() > 100) {
-//                     def dockerfile = 'Docs.Dockerfile'
-//                     def docsImage = docker.build("${registry}-release-notes:${BUILD_TAG}", "-f ${dockerfile} .")
 
-//                     docker.withRegistry( '', registryCredential ) {
-//                         docsImage.push()
-//                     }
-//                     sh "helm upgrade --install ${serviceName}-release-notes ${helmRepo}/${helmReleaseNote} --set image.repository=${registry}-release-notes --set image.tag=$BUILD_TAG --set fullnameOverride=${serviceName}-release-notes"
-
-//                     sh "docker rmi ${registry}-release-notes:${BUILD_TAG}"
-//                 } else {
-//                     sh "echo 'Release notes is not enough to release'"
-//                 }
-//             }
-//           }
-//         }
-//         stage('Update Helm') {
-//           steps{
-//               sh "helm upgrade --install ${serviceName} ${helmRepo}/${helmPackage} --set image.repository=${registry} --set image.tag=$BUILD_TAG --set fullnameOverride=${serviceName}-${helmPackage}"
-//           }
-//         }
-//         stage('Remove Unused docker image') {
-//           steps{
-//             sh "docker rmi ${registry}:${BUILD_TAG}"
-//           }
-//         }
+        stage('Remove Unused docker image') {
+          steps{
+            sh "docker rmi ${registry}:${gitCommit.GIT_COMMIT.take(7)}-${BUILD_NUMBER}"
+          }
+        }
       }
     }
 }
